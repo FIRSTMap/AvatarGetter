84b46b4f49db21797c1df360cc6187a69148a8a1
FishDog5000 <fishdog5000@gmail.com>
2019-08-15 16:50:22 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,300 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace AvatarGetter
+{
+    interface Avatar
+    {
+        void DrawToSpritesheet(Image<Rgba32> spriteSheet, int x, int y);
+        int TeamNumber { get; }
+    }
+
+    class Base64Avatar : Avatar
+    {
+        byte[] _Data;
+        public int TeamNumber { get; private set; }
+
+        public Base64Avatar(int teamNum, string base64)
+        {
+            TeamNumber = teamNum;
+            _Data = Convert.FromBase64String(base64);
+        }
+
+        public void DrawToSpritesheet(Image<Rgba32> spriteSheet, int x, int y)
+        {
+            Image<Rgba32> img;
+
+            try
+            {
+                // Just let ImageSharp detect the image codec. Don't specify that it is PNG.
+                // I found that when some people upload their avatars, instead of saving as
+                // .png, they save as a jpeg and then just change the file extension, so
+                // some of the avatars are actually JPEGs disguised as PNGs. Please, if you're
+                // reading this, upload your avatar as a PNG like FIRST tells you to, don't just
+                // upload a jpeg with a .png file extension.
+                img = Image.Load<Rgba32>(_Data);
+            }
+            catch
+            {
+                // Occasionally, ImageSharp can't handle the images (has not happened since I last updated it).
+                Console.WriteLine($"Failed to load avatar for {TeamNumber} with ImageSharp, decoding with System.Drawing.Common");
+
+                using MemoryStream input = new MemoryStream();
+                using MemoryStream output = new MemoryStream();
+
+                input.Write(_Data, 0, _Data.Length);
+                input.Position = 0;
+
+                System.Drawing.Bitmap.FromStream(input).Save(output, ImageFormat.Bmp);
+                output.Position = 0;
+                img = Image.Load<Rgba32>(output);
+            }
+
+            spriteSheet.Mutate(sheet =>
+                sheet.DrawImage(
+                    img,
+                    new Point(x, y),
+                    1
+                )
+            );
+
+            img.Dispose();
+        }
+    }
+
+    class SpriteAvatar : Avatar
+    {
+        public int TeamNumber { get; private set; }
+        private Image<Rgba32> _Sheet;
+        Loc _Loc;
+
+        public SpriteAvatar(int teamNum, Image<Rgba32> spriteSheet, Loc loc)
+        {
+            TeamNumber = teamNum;
+            _Sheet = spriteSheet;
+            _Loc = loc;
+        }
+
+        public void DrawToSpritesheet(Image<Rgba32> spriteSheet, int x, int y)
+        {
+            using Image<Rgba32> avatar = _Sheet.Clone();
+
+            avatar.Mutate(oldSheet =>
+                oldSheet.Crop(new Rectangle(_Loc.x, _Loc.y, Program.AVATAR_SIZE, Program.AVATAR_SIZE))
+            );
+
+            spriteSheet.Mutate(sheet =>
+                sheet.DrawImage(
+                    avatar,
+                    new Point(x, y),
+                    1
+                )
+            );
+        }
+    }
+
+    // A team from teams.json
+    class Team
+    {
+        public int team_number;
+        public double lat;
+        public double lng;
+    }
+
+    // Represents the information in avatars.json
+    class AvatarInfo
+    {
+        public int sheet_size;
+        public Dictionary<int, Loc> locations;
+
+        public AvatarInfo(int sheetSize)
+        {
+            sheet_size = sheetSize;
+            locations = new Dictionary<int, Loc>();
+        }
+    }
+
+    // Location of a team's avatar in the sprite sheet
+    struct Loc
+    {
+        public int x;
+        public int y;
+
+        public Loc(int xIn, int yIn)
+        {
+            x = xIn;
+            y = yIn;
+        }
+    }
+
+    class Program
+    {
+        private static readonly HttpClient _Client = new HttpClient();
+        private static HashSet<int> _ValidTeams;
+
+        // All FRC avatars are 40x40.
+        public const int AVATAR_SIZE = 40;
+
+        static async Task<List<Avatar>> DownloadAvatars()
+        {
+            Console.WriteLine("Downloading avatars from the FRC API (https://frc-events.firstinspires.org/services/API)");
+
+            string FRC_API_KEY = Convert.ToBase64String(File.ReadAllBytes("FRC_API_KEY"));
+            string YEAR;
+
+            // YEAR file can be used to specify the year to get avatars for
+            if (File.Exists("YEAR"))
+            {
+                YEAR = File.ReadAllText("YEAR").Trim();
+            }
+            else
+            {
+                Console.WriteLine("YEAR file does not exist, getting year from DateTime.");
+                YEAR = DateTime.Now.Year.ToString();
+            }
+
+            _Client.DefaultRequestHeaders.Add("Authorization", "Basic " + FRC_API_KEY);
+            _Client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            int page = 1;
+            int pageTotal = 99;
+
+            List<Avatar> avatars = new List<Avatar>();
+
+            while (page <= pageTotal)
+            {
+                HttpResponseMessage msg = await _Client.GetAsync("https://frc-api.firstinspires.org/v2.0/" + YEAR + "/avatars?page=" + page.ToString());
+
+                string jsonStr = await msg.Content.ReadAsStringAsync();
+
+                JObject obj = JObject.Parse(jsonStr);
+
+                JArray teams = (JArray)obj["teams"];
+
+                foreach (JObject team in teams)
+                {
+                    int teamNum = team.Value<int>("teamNumber");
+
+                    if (_ValidTeams.Contains(teamNum))
+                    {
+                        if (team.TryGetValue("encodedAvatar", out JToken avatar))
+                        {
+                            string base64 = avatar.Value<string>();
+
+                            if (base64 != null)
+                            {
+                                _ValidTeams.Remove(teamNum);
+                                avatars.Add(new Base64Avatar(teamNum, base64));
+                            }
+                        }
+                    }
+                }
+
+                pageTotal = obj.Value<int>("pageTotal");
+                Console.WriteLine($"Downloaded page {page}/{pageTotal}");
+
+                page++;
+            }
+
+            return avatars;
+        }
+
+        private static async Task GetAllAvatars(string dataFolder)
+        {
+            // Get a list of all the teams on the map
+            Team[] teamData = JsonConvert.DeserializeObject<Team[]>(File.ReadAllText(
+                Path.Combine(dataFolder, "teams.json")));
+            _ValidTeams = teamData.Select(team => team.team_number).ToHashSet();
+
+            // Download all of the current avatars
+            List<Avatar> avatars = await DownloadAvatars();
+
+            string oldAvatarsDataFile = Path.Combine(dataFolder, "avatars.json");
+            string oldAvatarsSheetFile = Path.Combine(dataFolder, "avatars.png");
+
+            // Load all the old avatars
+            if (!File.Exists(oldAvatarsDataFile) || !File.Exists(oldAvatarsSheetFile))
+            {
+                Console.WriteLine("Warning: old avatars.json or avatars.png not found in data folder.");
+                Console.WriteLine("Skipping import of old avatars.");
+            }
+            else
+            {
+                AvatarInfo oldAvatars = JsonConvert.DeserializeObject<AvatarInfo>(File.ReadAllText(oldAvatarsDataFile));
+                Image<Rgba32> oldSheet = Image.Load<Rgba32>(oldAvatarsSheetFile);
+
+                foreach (KeyValuePair<int, Loc> teamLocPair in oldAvatars.locations)
+                {
+                    // Only include avatars that do not have new avatars downloaded
+                    if (_ValidTeams.Contains(teamLocPair.Key))
+                    {
+                        _ValidTeams.Remove(teamLocPair.Key);
+
+                        avatars.Add(new SpriteAvatar(teamLocPair.Key, oldSheet, teamLocPair.Value));
+                    }
+
+                    if (_ValidTeams.Count == 0)
+                        break;
+                }
+            }
+
+            int sheetSize = (int)Math.Ceiling(Math.Sqrt(avatars.Count));
+            Image<Rgba32> sheet = new Image<Rgba32>(sheetSize * AVATAR_SIZE, sheetSize * AVATAR_SIZE);
+
+            // The (new) avatar locations in the spritesheet
+            AvatarInfo avatarInfo = new AvatarInfo(sheetSize);
+
+            Console.WriteLine("Stitching " + avatars.Count + " avatars...");
+
+            for (int i = 0; i < avatars.Count; i++)
+            {
+                Avatar avatar = avatars[i];
+                int x = (i % sheetSize) * AVATAR_SIZE;
+                int y = (i / sheetSize) * AVATAR_SIZE;
+
+                avatar.DrawToSpritesheet(sheet, x, y);
+                avatarInfo.locations.Add(avatar.TeamNumber, new Loc(x, y));
+            }
+
+            Console.WriteLine("Saving...");
+
+            using (StreamWriter sw = new StreamWriter(File.Open("avatars.json", FileMode.Create)))
+                sw.Write(JsonConvert.SerializeObject(avatarInfo));
+
+            using (FileStream fs = File.Open("avatars.png", FileMode.Create))
+                sheet.SaveAsPng(fs);
+
+            Console.WriteLine("Done!");
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Error: wrong number of arguments: " + args.Length);
+                Console.WriteLine("Usage: image-stitcher [firstmap data directory]");
+                Console.WriteLine("Generates a spritesheet from images that are all the same size and are square.");
+                return;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Error: data directory " + args[0] + " does not exist!");
+                return;
+            }
+
+            GetAllAvatars(args[0]).Wait();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+# FIRSTMap Avatar Getter
+This program downloads FRC avatars from the FRC API
+([https://frc-events.firstinspires.org/services/API](https://frc-events.firstinspires.org/services/API))
+and stores them in a sprite sheet.
+
+It must be called with the FIRSTMap `data` folder as an
+argument. It uses the `teams.json` as a list of the
+teams to save avatars for (if avatars for those teams
+are available) and skips all other avatars. If there
+is no avatar available for a team from the FRC API, but
+there is one (most likely from a previous year) in a
+previously generated avatar sprite sheet in the FIRSTMap
+data directory, it will be copied to the new sprite sheet.
+For example, if team 1234 does not have an avatar for the
+2019 season, but there is an avatar in the `avatars.png` /
+`avatars.json` sprite sheet in the FIRSTMap data directory
+(left over from the 2018 season), it will be copied to the
+new sprite sheet that is being generated.
+
+## Usage:
+Create a file called `FRC_API_KEY` in the root of the
+repository and add your FRC API key in the following
+format:
+```
+api_username:api-key-guid
+```
+See
+[https://frcevents2.docs.apiary.io/#/reference/authorization](https://frcevents2.docs.apiary.io/#/reference/authorization)
+for more info about FRC API key formatting. The program
+will automatically base64 encode the key when it loads it.
+
+FRC API keys cannot be redistributed, so an API key cannot
+be included in the repository. An FRC API key can be
+obtained from
+[https://frc-events.firstinspires.org/services/API](https://frc-events.firstinspires.org/services/API).
+
+The `YEAR` file can be used to specify what
+year to get avatars for. Otherwise, it will get the
+current year using the system time.
+
+
+The program can be run after installing the .NET Core SDK
+(version 3.0 Preview 8 or later).
+
+Run the program by executing:
+```
+dotnet run [firstmap-data-folder]
+```
+
+The program creates an `avatars.png` and `avatars.json` in
+the project root. Copy these to the FIRSTMap `data` folder
+and replace the old avatars sprite sheet.
